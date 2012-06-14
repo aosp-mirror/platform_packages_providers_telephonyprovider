bf7c617b88876d8255590de1c855a6edeaa7dad2
Wink Saville <wink@google.com>
2012-06-14 12:43:28 -0700

    Use telephony-common and mms-common
    Change-Id: I5378acae02693356ca4a0546cd9346910c29d9dc

--- Android.mk
+++ Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := TelephonyProvider
 LOCAL_CERTIFICATE := platform
 
+LOCAL_JAVA_LIBRARIES += telephony-common mms-common
 LOCAL_STATIC_JAVA_LIBRARIES += android-common
 
 include $(BUILD_PACKAGE)
