9a7cea114451acbd3166c5778e2bcc74f85372e7
Jordan Liu <jminjie@google.com>
2017-11-16 14:45:07 -0800

    Fix handling of URIs with subId
    Bug: 69419776
    Test: manual and TelephonyProviderTest.java unit tests
    Change-Id: Ib0e053be5a2f00179037e0df09ff1347b3443cb4
    Merged-In: Ib0e053be5a2f00179037e0df09ff1347b3443cb4

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -1991,6 +1991,8 @@ public class TelephonyProvider extends ContentProvider
         qb.setStrict(true); // a little protection from injection attacks
         qb.setTables(CARRIERS_TABLE);
 
+        List<String> constraints = new ArrayList<String>();
+
         int match = s_urlMatcher.match(url);
         switch (match) {
             case URL_TELEPHONY_USING_SUBID: {
@@ -2002,14 +2004,12 @@ public class TelephonyProvider extends ContentProvider
                     return null;
                 }
                 if (DBG) log("subIdString = " + subIdString + " subId = " + subId);
-                qb.appendWhere(NUMERIC + " = '" + mTelephonyManager.getSimOperator(subId) + "'");
-                // FIXME alter the selection to pass subId
-                // selection = selection + "and subId = "
+                constraints.add(NUMERIC + " = '" + mTelephonyManager.getSimOperator(subId) + "'");
+                constraints.add(SUBSCRIPTION_ID + "=" + subIdString);
             }
             // intentional fall through from above case
-            // do nothing
             case URL_TELEPHONY: {
-                qb.appendWhere(NOT_OWNED_BY_DPC);
+                constraints.add(NOT_OWNED_BY_DPC);
                 break;
             }
 
@@ -2022,21 +2022,20 @@ public class TelephonyProvider extends ContentProvider
                     return null;
                 }
                 if (DBG) log("subIdString = " + subIdString + " subId = " + subId);
-                // FIXME alter the selection to pass subId
-                // selection = selection + "and subId = "
+                constraints.add(SUBSCRIPTION_ID + "=" + subIdString);
             }
             //intentional fall through from above case
             case URL_CURRENT: {
-                qb.appendWhere("current IS NOT NULL");
-                qb.appendWhere(NOT_OWNED_BY_DPC);
+                constraints.add("current IS NOT NULL");
+                constraints.add(NOT_OWNED_BY_DPC);
                 // do not ignore the selection since MMS may use it.
                 //selection = null;
                 break;
             }
 
             case URL_ID: {
-                qb.appendWhere("_id = " + url.getPathSegments().get(1));
-                qb.appendWhere(NOT_OWNED_BY_DPC);
+                constraints.add("_id = " + url.getPathSegments().get(1));
+                constraints.add(NOT_OWNED_BY_DPC);
                 break;
             }
 
@@ -2050,11 +2049,12 @@ public class TelephonyProvider extends ContentProvider
                     return null;
                 }
                 if (DBG) log("subIdString = " + subIdString + " subId = " + subId);
+                constraints.add(SUBSCRIPTION_ID + "=" + subIdString);
             }
             //intentional fall through from above case
             case URL_PREFERAPN:
             case URL_PREFERAPN_NO_UPDATE: {
-                qb.appendWhere("_id = " + getPreferredApnId(subId, true));
+                constraints.add("_id = " + getPreferredApnId(subId, true));
                 break;
             }
 
@@ -2066,6 +2066,11 @@ public class TelephonyProvider extends ContentProvider
             default: {
                 return null;
             }
+        }
+
+        // appendWhere doesn't add ANDs so we do it ourselves
+        if (constraints.size() > 0) {
+            qb.appendWhere(TextUtils.join(" AND ", constraints));
         }
 
         if (match != URL_SIMINFO) {
@@ -2208,6 +2213,8 @@ public class TelephonyProvider extends ContentProvider
                 } else {
                     values = new ContentValues();
                 }
+
+                values.put(SUBSCRIPTION_ID, subId);
 
                 values = DatabaseHelper.setDefaultValue(values);
                 if (!values.containsKey(EDITED)) {
