8e0a13e0a6fd6ad00fc52bb7595848f685358534
Malcolm Chen <refuhoo@google.com>
2019-02-19 19:43:38 -0800

    Don't return hidden subscription in getActiveSubIdList
    Now getActiveSubscriptionInfoList will not return hidden
    subscriptions. And getActiveSubIdList should be consistent
    with it. For CarrierProvider, it should call the internal
    API to see all subscriptions including hidden ones.
    
    Bug: 124690005
    Test: manual
    Change-Id: Ic208ab93dbaba78d359a66594be9d87b56560c3a

--- src/com/android/providers/telephony/CarrierIdProvider.java
+++ src/com/android/providers/telephony/CarrierIdProvider.java
@@ -604,7 +604,7 @@ public class CarrierIdProvider extends ContentProvider {
         if (!SubscriptionController.getInstance().isActiveSubId(subId)) {
             // Remove absent subId from the currentSubscriptionMap.
             final List activeSubscriptions = Arrays.asList(SubscriptionController.getInstance()
-                    .getActiveSubIdList());
+                    .getActiveSubIdList(false));
             int count = 0;
             for (int subscription : mCurrentSubscriptionMap.keySet()) {
                 if (!activeSubscriptions.contains(subscription)) {
