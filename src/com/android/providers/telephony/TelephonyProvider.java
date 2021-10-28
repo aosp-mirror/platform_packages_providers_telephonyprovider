064d348d7fb94bca047c16fdc38d805cca062eac
Grace Jia <xiaotonj@google.com>
2021-10-27 17:14:16 -0700

    Fix typo in TelephonyProvider.
    Fix it in new version 52 to make sure user upgraded to 51 can get this
    fix properly.
    
    Test: unit test
    Change-Id: I3697789054536a58d90483dd7dfe336f55a49b46

--- src/com/android/providers/telephony/TelephonyProvider.java
+++ src/com/android/providers/telephony/TelephonyProvider.java
@@ -155,7 +155,7 @@ public class TelephonyProvider extends ContentProvider
     private static final boolean DBG = true;
     private static final boolean VDBG = false; // STOPSHIP if true
 
-    private static final int DATABASE_VERSION = 52 << 16;
+    private static final int DATABASE_VERSION = 53 << 16;
     private static final int URL_UNKNOWN = 0;
     private static final int URL_TELEPHONY = 1;
     private static final int URL_CURRENT = 2;
@@ -1704,6 +1704,21 @@ public class TelephonyProvider extends ContentProvider
                 }
                 oldVersion = 52 << 16 | 6;
             }
+
+            if (oldVersion < (53 << 16 | 6)) {
+                try {
+                    // Try to update the siminfo table. Fix typo error in version 51.
+                    db.execSQL("ALTER TABLE " + SIMINFO_TABLE + " ADD COLUMN "
+                            + Telephony.SimInfo.COLUMN_D2D_STATUS_SHARING_SELECTED_CONTACTS
+                            + " TEXT;");
+                } catch (SQLiteException e) {
+                    if (DBG) {
+                        log("onUpgrade failed to updated " + SIMINFO_TABLE
+                                + " to add d2d status sharing contacts. ");
+                    }
+                }
+            }
+
             if (DBG) {
                 log("dbh.onUpgrade:- db=" + db + " oldV=" + oldVersion + " newV=" + newVersion);
             }
@@ -3508,7 +3523,7 @@ public class TelephonyProvider extends ContentProvider
                 PersistableBundle backedUpSimInfoEntry, int backupDataFormatVersion,
                 String isoCountryCodeFromDb,
                 List<String> wfcRestoreBlockedCountries) {
-            if (DATABASE_VERSION != 52 << 16) {
+            if (DATABASE_VERSION != 53 << 16) {
                 throw new AssertionError("The database schema has been updated which might make "
                     + "the format of #BACKED_UP_SIM_SPECIFIC_SETTINGS_FILE outdated. Make sure to "
                     + "1) review whether any of the columns in #SIM_INFO_COLUMNS_TO_BACKUP have "
